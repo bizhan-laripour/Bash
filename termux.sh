27e7713d46e1bebaed8a2329cf8af80928d557b4
Bizhan laripour <41441280+bizhan-laripour@users.noreply.github.com>
2024-02-24 10:04:24 +0330

    Update termux.sh

--- termux.sh
+++ termux.sh
@@ -1,1 +1,8 @@
-this file contains termux commands
+#!/bin/bash
+string="hello , world , how , are , you , ?"
+IFS=','
+read -ra array <<< "$string"
+for val in "${array[@]}";
+do
+figlet "$val"
+done
