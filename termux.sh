5d47ac6986996cf1ed1a119c450b1fd10371d848
Bizhan laripour <41441280+bizhan-laripour@users.noreply.github.com>
2024-02-25 10:16:34 -0500

    Update termux.sh

--- termux.sh
+++ termux.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
+clear
 string="hello , world , how , are , you , ?"
+translation="salam,chetori"
 IFS=','
 read -ra array <<< "$string"
 for val in "${array[@]}";
 do
 figlet "$val"
+sleep 3
+done
+
+echo do you want translation? (y/n)
+IFS=',' read -ra trans <<< "$translation"
+for tr in "${trans[@]}"; 
+do
+figlet "$tr"
+sleep 3
 done
