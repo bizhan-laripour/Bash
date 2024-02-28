147aca2a543986a78ce6e1b5e4b3b01212366631
Bizhan laripour <41441280+bizhan-laripour@users.noreply.github.com>
2024-02-28 16:46:24 +0330

    Update inputToList.sh

--- inputToList.sh
+++ inputToList.sh
@@ -1,28 +1,27 @@
-#!/bin/usr/env bash
-# this bash file doesnt work acurately and must be changed
 declare -a sportArray
 flag="false"
 for ((i=0 ; i<10; i++));do
 echo please enter a sport:
-read input
+read -r input
 
-for val in ${sportArray[@]};
+for val in "${sportArray[@]}";
 do
-        if [ $val == s ];then
-                echo this sport is already added
-                flag= "true"
+        if [ "$val" == "$input" ];then
+                echo "this sport is already added"
+                flag="true"
                 break
+
         fi
 done
 if [ $flag == "false" ]; then
-sportArray+=($input)
-flag="false"
+sportArray+=("$input")
 
 fi
+flag="false"
 done
 
 echo "${#sportArray[@]}"
-for val in ${sportArray[@]};
+for val in "${sportArray[@]}";
 do
         echo -e "$val \n"
 done
