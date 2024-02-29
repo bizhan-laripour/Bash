1cff2ee95de33002ba1f2789e4006cd3ad7752e8
Bizhan laripour <41441280+bizhan-laripour@users.noreply.github.com>
2024-03-01 03:26:10 +0330

    Update RMM.sh

--- RMM.sh
+++ RMM.sh
@@ -6,25 +6,22 @@ YELLOW='\033[01;33m'
 GREEN='\033[01;32m'  
 echo -e
 ${bold} ${NONE}Hello from ${RED}RMM${NONE}. Im ${GREEN}Bizhan Laripour ${NONE}and wish you enjoy with this project.                                                                                                                                                                                                                             "                                                    
-declare -a exist=()   
-if test -d ./RMM_CONFIG_REPO;then 
-exist+=(RMM_CONFIG_REPO)   
-fi       
-if test -d ./RMM_DISCOVERY;then 
-exist+=(RMM_DISCOVERY)  
-fi    
-if test -d ./RMM_CONFIG_SERVER;then 
-exist+=(RMM_CONFIG_SERVER) 
-fi      
-if test -d ./RMM_AGENT;then  
-exist+=(RMM_AGENT) 
-fi
-if test -d ./RMM_WORKER;then                          
-exist+=(RMM_WORKER)    
-fi
-for ((i=0;i<"${#exist[@]}"; i++));do                            
-number=$(( $i + 1 ))                            
-echo $number : "${exist[$i]}" exist                   
+declare -a all=(   
+RMM_CONFIG_REPO     
+RMM_DISCOVERY       
+RMM_CONFIG_SERVER      
+RMM_WORKER      
+RMM_AGENT      
+)
+declare -a exist=()    
+for val in "${all[@]}";do       
+if test -d ./$val;then         
+exist+=($val)              
+fi                         
+done
+for ((i=0;i<"${#exist[@]}"; i++));do  
+number=$(( $i + 1 ))    
+echo $number : "${exist[$i]}" exist   
 done
 echo "which module do you want to replace from git? just enter number of module with comma seperate:"
 read -r modules
