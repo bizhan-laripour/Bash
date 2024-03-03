edab63fc195371c9b7fb6749c5dedf859ab1d015
Bizhan laripour <41441280+bizhan-laripour@users.noreply.github.com>
2024-03-03 14:41:07 +0330

    Update snake.sh

--- snake.sh
+++ snake.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
   clear
-  for ((i=0;i<30;i++)); do
-    for ((j=0;j<30;j++)); do
-      if [[ $i -eq 0 || $i -eq 29 || $j -eq 0 || $j -eq 29 ]]; then
+  for ((i=0;i<20;i++)); do
+    for ((j=0;j<40;j++)); do
+      if [[ $i -eq 0 || $i -eq 19 || $j -eq 0 || $j -eq 39 ]]; then
         echo -n "#"
+        sleep 0.01
       else
         echo -n " "
+        sleep 0.01
       fi
     done
     echo
