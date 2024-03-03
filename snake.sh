d80fc96109a6aac47a4aba6651a5ffae23bb7c65
Bizhan laripour <41441280+bizhan-laripour@users.noreply.github.com>
2024-03-03 14:51:01 +0330

    Update snake.sh

--- snake.sh
+++ snake.sh
@@ -5,10 +5,16 @@
       if [[ $i -eq 0 || $i -eq 19 || $j -eq 0 || $j -eq 39 ]]; then
         echo -n "#"
         sleep 0.01
+
+        elif [[ $i -eq 5 && $j -eq 5 ]]; then
+            echo -n "@@@"
+        elif [[ $i -eq 10 && $j -eq 10 ]]; then
+            echo -n "@"
       else
         echo -n " "
         sleep 0.01
       fi
     done
-    echo
+    echo 
   done
+  echo "score:"
