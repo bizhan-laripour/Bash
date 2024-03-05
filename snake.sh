e7ad3631e529c447773a48b1694688d7aada8583
Bizhan laripour <41441280+bizhan-laripour@users.noreply.github.com>
2024-03-05 20:49:38 +0330

    Update snake.sh

--- snake.sh
+++ snake.sh
@@ -1,46 +1,41 @@
 #!/bin/bash
 
-worm="@@@"
-apple="@"
-direction="o"
+worm="##"
+apple="@"                                               direction=d
 idirection=5
-jDirection=5
-  declare -a keys=( w a s d)
+jDirection=5                                            iappleDirection=10                                      jappleDirection=10
+gameOver=false
 
-  function display {
+function display {
     clear
        for ((i=0;i<20;i++)); do
           for ((j=0;j<40;j++)); do
             if [[ $i -eq 0 || $i -eq 19 || $j -eq 0 || $j -eq 39 ]]; then
               echo -n "#"
-
-
-              elif [[ $i -eq $idirection && $j -eq $jDirection ]]; then
-                  echo -n $worm
-              elif [[ $i -eq 10 && $j -eq 10 ]]; then
+              elif [[ $i -eq $idirection && $j -eq $jDirection ]]; then                                                        
+echo -n $worm                                    
+elif [[ $i -eq $iappleDirection && $j -eq $jappleDirection ]]; then
                   echo -n $apple
-            else
-              echo -n " "
-
-            fi
+            else                                                   
+            echo -n " "
+            fi                                               
+            done
+          echo                                            
           done
-          echo
-        done
-        echo "score:"
+        echo "score:$score"
   }
 
-
-  function change_direction {
-            if [ $direction == w ]; then
-              idirection=$idirection-1
+function change_direction {
+            if [ $direction == w ]; then                            
+            idirection=$(($idirection -1))               
             elif [ $direction == a ];then
-              jDirection=$jDirection-1
-            elif [ $direction == s ];then
-              idirection=$idirection+1
+                    jDirection=$(($jDirection -1))             
+                    elif [ $direction == s ];then
+                    idirection=$(($idirection +1))
             elif [ $direction == d ]; then
-                jDirection=$jDirection+1
+                    jDirection=$(($jDirection +1))
             fi
-            sleep 0.9
+            sleep 0.2
   }
 
   function random_apple {
@@ -58,21 +53,27 @@ jDirection=5
                 score=$(( $score+1 ))
                 random_apple
           fi
-
   }
+function game_over {                                  
+  if [[ $idirection == 19 || $jDirection == 39 || $idirection == 0 || $jDirection == 0 ]];then
+
+          echo "game over!!"
+          echo "final score is $score"
+          gameOver=true                                 
+  fi
 
 while  true;do
-  display
-  read -t 0.1  -sn1 dir
-  if [[ "$dir" == w || "$dir" == a || "$dir" == s || "$dir" == d ]];then
-         direction=$dir
-
-  fi
-  change_direction
-  colided
-   
+        if [ $gameOver == false ];then
+           display
+           read -t 0.1  -sn1 dir
+           if [[ "$dir" == w || "$dir" == a || "$dir" == s || "$dir" == d ]];then
+           direction=$dir
+           fi
+           change_direction
+           colided
+           game_over                                         
+           fi
 done
-
 
 
 
