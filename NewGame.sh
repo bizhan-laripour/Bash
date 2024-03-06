0228fdad93b3cb8c4715d1d0975e5fe04fd94404
Bizhan laripour <41441280+bizhan-laripour@users.noreply.github.com>
2024-03-06 04:00:59 +0330

    Create NewGame.sh

---
+++ NewGame.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+                                                                                                              
+                                                                                                             
+declare -a shapes=(                                    
+"@@@@"
+"@@@                                                     
+ @@@
+ "
+ "@@@                                                     
+  @
+  "                                                       
+  "@@@@
+   @"
+  "@
+   @                                                     
+   @                                                     
+   @"
+)                                                                                                               
+function display {
+        clear
+
+for (( i=0; i<30; i++ ));do
+for (( j=0; j<50;j++ ));do
+if [[ $i -eq 0 || $i -eq 29 || $j -eq 0 || $j -eq 49 ]];
+then                                                           
+echo -n "#"                                  
+else
+echo -n  " "
+fi
+    done
+     echo
+done
+
+}
+
+display
