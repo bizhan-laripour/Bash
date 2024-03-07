8a8d635ff7a90a9d37d6100af69d7634b6ec10c1
Bizhan laripour <41441280+bizhan-laripour@users.noreply.github.com>
2024-03-08 02:00:11 +0330

    Update Atari.sh

--- Atari.sh
+++ Atari.sh
@@ -1,23 +1,37 @@
-#!/bin/bash
-                                                                                                              
-                                                                                                             
-declare -a shapes=(                                                                                        
- "@@"
- "¥^¥"
- "€&€"
- "¶∆¶"
-)  
-tir="¥"
-exploid="##"
-airplain="<[∆]>"
+#!/bin/bash                                           
+declare -a shapes=(                                                                                            
+"@@"                                                 
+"¥^¥"                                                
+"€&€"                                                 
+"¶∆¶"                                               
+)                                                      
+tir="¥"                                               
+exploid="##"                                          
+airplain="<[∆]>"                                    
+RANDOM=$$$(date +%s)                               
+
+time= 0                                               
+iPlainDirection=27                                    
+jPlainDirection=23
+iEnemy=1                                            
+jEnemy=23                                               
+input=p
+shape=""
 function display {
         clear
 
 for (( i=0; i<30; i++ ));do
 for (( j=0; j<50;j++ ));do
 if [[ $i -eq 0 || $i -eq 29 || $j -eq 0 || $j -eq 49 ]];
-then                                                           
-echo -n "#"                                  
+then                                                    
+echo -n "#"                                                                                                   
+elif [[ $i -eq $iPlainDirection && $j -eq $jPlainDirection ]];then
+        echo -n $airplain
+elif [[ $i -eq $iEnemy && $j -eq $jEnemy && $iEnemy != 29 ]];then
+        echo -n $shape
+elif [ $iEnemy -eq 29 ];then
+        time=0
+
 else
 echo -n  " "
 fi
@@ -26,5 +40,3 @@ fi
 done
 
 }
-
-display
